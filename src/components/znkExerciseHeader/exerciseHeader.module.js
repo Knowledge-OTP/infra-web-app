058372d58d2781c937eb442ea05e211f6020f3f8
Oded <oded.sharir@gmail.com>
2016-05-30 09:57:28 +0300

    remove my exercise header

--- src/components/znkExerciseHeader/exerciseHeader.module.js
+++
@@ -1,6 +0,0 @@
-import { exerciseHeaderDrv } from './exerciseHeader.directive.js';
-import { exerciseHeaderController } from './exerciseHeader.controller.js';
-
-angular.module('actWebApp')
-    .directive('znkExerciseHeader', exerciseHeaderDrv)
-    .controller('exerciseHeaderController', exerciseHeaderController);
