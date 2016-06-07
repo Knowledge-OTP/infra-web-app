7a68ea2fed1846fc636e52248754479ea53f781a
Oded <oded.sharir@gmail.com>
2016-06-07 18:39:55 +0300

    add content to new firebase

--- dist/main.js
+++ dist/main.js
@@ -2104,7 +2104,7 @@ angular.module('znk.infra-web-app.invitation').run(['$templateCache', function($
         'ENV', '$http', '$window',
         function (ENV, $http, $window) {
             this.login = function(loginData){
-                var ref = new Firebase(ENV.fbGlobalEndPoint);
+                var ref = new Firebase(ENV.fbGlobalEndPoint, ENV.firebaseAppScopeName);
                 return ref.authWithPassword(loginData).then(function(authData){
                     var postUrl = ENV.backendEndpoint + 'firebase/token';
                     var postData = {
@@ -2117,7 +2117,7 @@ angular.module('znk.infra-web-app.invitation').run(['$templateCache', function($
                     };
 
                     return $http.post(postUrl, postData).then(function (token) {
-                        var refDataDB = new Firebase(ENV.fbDataEndPoint);
+                        var refDataDB = new Firebase(ENV.fbDataEndPoint, ENV.firebaseAppScopeName);
                         refDataDB.authWithCustomToken(token.data).then(function(){
                             var appUrl = ENV.redirectLogin;
                             $window.location.replace(appUrl);
