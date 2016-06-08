96db8933a5cd288683b4f137acb9d054c689f241
odeds <oded.sharir@gmail.com>
2016-06-08 11:53:54 +0300

    oded/tfl-962-changes (#12)
    * image path change
    
    * add content to new firebase
    
    * added to env demoShared

--- demo/loginForm/demo.js
+++ demo/loginForm/demo.js
@@ -16,4 +16,5 @@ angular.module('demo', ['znk.infra-web-app.loginForm'])
         this.fbDataEndPoint = "https://act-dev.firebaseio.com/";
         this.dataAuthSecret = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicmFjY29vbnMifQ.mqdcwRt0W5v5QqfzVUBfUcQarD0IojEFNisP-SNIFLM";
         this.redirectLogin = "http://dev-act.zinkerz.com.s3-website-eu-west-1.amazonaws.com/";
+        this.firebaseAppScopeName = "act_app";
     });
