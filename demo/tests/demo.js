bf56f98cba75d3d914287037c84a7479ad960f40
Allen Jacobson <allen@zinkerz.com>
2016-06-22 18:30:32 +0300

    saving work

--- demo/tests/demo.js
+++ demo/tests/demo.js
@@ -14,9 +14,6 @@ angular.module('demo', [
             .preferredLanguage('en');
 
         var svgMap = {
-            // 'tests-check-mark-icon': 'svg/check-mark-icon.svg',
-            // 'tests-locked-icon': 'svg/locked-icon.svg',
-            // 'tests-subject-locked-icon': 'svg/subject-locked-icon.svg'
         };
         SvgIconSrvProvider.registerSvgSources(svgMap);
     })
