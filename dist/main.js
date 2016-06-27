9a9d9cbbbab0d653166e792907dcf31bc755082c
igormas <igor.masliansky@gmail.com>
2016-06-27 16:18:17 +0300

    hotfix

--- dist/main.js
+++ dist/main.js
@@ -20,6 +20,13 @@
     ]);
 })(angular);
 
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-web-app.angularMaterialOverride', [
+        'ngMaterial'
+    ]);
+})(angular);
 
 angular.module('znk.infra-web-app.angularMaterialOverride').run(['$templateCache', function($templateCache) {
 
