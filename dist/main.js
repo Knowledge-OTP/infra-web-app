c5202dd5d57257fe96344c409663bbaffa1f6396
Igor Masliansky <igormas@users.noreply.github.com>
2016-06-27 16:24:54 +0300

    Igor/sass infra (#30)
    * hotfix
    
    * hotfix

--- dist/main.js
+++ dist/main.js
@@ -28,6 +28,18 @@ angular.module('znk.infra-web-app.angularMaterialOverride').run(['$templateCache
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra-web-app.angularMaterialOverride', [
+        'ngMaterial'
+    ]);
+})(angular);
+
+angular.module('znk.infra-web-app.angularMaterialOverride').run(['$templateCache', function($templateCache) {
+
+}]);
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra-web-app.config', []).config([
         function(){}
     ]);
