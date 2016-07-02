8816804c93b983582b85d28b7aa7eb08ad2d606c
kosta mas <kosta.masliansky@gmail.com>
2016-07-02 18:55:36 +0300

    logout

--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -30,7 +30,6 @@ module.exports = function (config) {
             'bower_components/firebase/firebase.js',
             'bower_components/angular-translate/angular-translate.js',
             'bower_components/angular-translate-loader-partial/angular-translate-loader-partial.js',
-            'bower_components/angularfire/dist/angularfire.js',
             'bower_components/angular-svg-round-progressbar/build/roundProgress.min.js',
             'bower_components/infra/dist/main.js',
             'bower_components/angular-ui-router/release/angular-ui-router.js',
