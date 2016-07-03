264a26702a82e0d9ce3c87472b15eaaf688e8e65
Igor Masliansky <igormas@users.noreply.github.com>
2016-07-03 15:48:42 +0300

    Igor/in app messages (#37)
    * saving work
    
    * saving work
    
    * iap message raccoon was added to infra web app
    
    * iap message raccoon was added to infra web app

---
+++ src/components/iapMsg/module.js
@@ -0,0 +1,18 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-web-app.iapMsg',[
+        'ngSanitize',
+        'znk.infra.svgIcon',
+        'ngAnimate'
+    ])
+        .config(function(SvgIconSrvProvider){
+            'ngInject'; 
+
+            var svgMap = {
+                'iap-msg-close-msg': 'components/iapMsg/svg/close-msg.svg',
+                'iap-msg-hint-bubble': 'components/iapMsg/svg/hint-bubble.svg'
+            };
+            SvgIconSrvProvider.registerSvgSources(svgMap);
+        });
+})(angular);
