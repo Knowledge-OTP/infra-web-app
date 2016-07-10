3179f99d3cb48f7e514a2030a645473ef430f3b3
Igor <igor.masliansky@gmail.com>
2016-07-10 21:08:01 +0300

    dist

--- dist/tests/tests.js
+++ dist/tests/tests.js
@@ -34,7 +34,7 @@
     'use strict';
 
     angular.module('znk.infra-web-app.tests').directive('navigationPane',
-        ["$translatePartialLoader", "ExamTypeEnum", "ExamSrv", "ExerciseResultSrv", function ($translatePartialLoader, ExamTypeEnum, ExamSrv, ExerciseResultSrv) {
+        ["$translatePartialLoader", "ExamTypeEnum", "ExamSrv", "ExerciseResultSrv", "$q", function ($translatePartialLoader, ExamTypeEnum, ExamSrv, ExerciseResultSrv, $q) {
             'ngInject';
             return {
                 scope: {
@@ -53,14 +53,42 @@
 
                     ExamSrv.getAllExams().then(function(examsArr){
                         var examArr = [];
+                        var getExamResultPromArr = [];
 
                         angular.forEach(examsArr, function (exam) {
                             var examCopy = angular.copy(exam);
                             examArr.push(examCopy);
 
-                            ExerciseResultSrv.getExamResult(exam.id, true).then(function(examResult){
+                            var getExamResultProm = ExerciseResultSrv.getExamResult(exam.id, true).then(function(examResult){
                                 examCopy.isCompleted = !!(examResult && examResult.isCompleted);
                             });
+
+                            getExamResultPromArr.push(getExamResultProm);
+                        });
+                        //set active exam id
+                        $q.all(getExamResultPromArr).then(function(){
+                            var activeExamId;
+
+                            for(var i=0; i<examArr.length; i++){
+                                var exam = examArr[i];
+
+                                if(exam.typeId === ExamTypeEnum.MINI_TEST.enum){
+                                    activeExamId = exam.id;
+                                    break;
+                                }
+                                //active exam id is already set with higher order full test
+                                if(angular.isDefined(activeExamId)){
+                                    continue;
+                                }
+
+                                activeExamId = exam.id;
+                            }
+                            //all exams are completed
+                            if(angular.isUndefined(activeExamId)){
+                                activeExamId = examArr[0].id;
+                            }
+
+                            scope.vm.changeActive(activeExamId);
                         });
 
                         scope.vm.examArr = examArr;
