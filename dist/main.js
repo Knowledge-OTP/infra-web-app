e6fd73f6fa414f91cc8e750554b4c262283e34be
igormas <igor.masliansky@gmail.com>
2016-08-21 17:02:03 +0300

    saving work

--- dist/main.js
+++ dist/main.js
@@ -786,7 +786,7 @@ angular.module('znk.infra-web-app.completeExercise').run(['$templateCache', func
     "        <div ng-switch-when=\"2\" class=\"animate-view-state\">\n" +
     "            <complete-exercise-exercise></complete-exercise-exercise>\n" +
     "        </div>\n" +
-    "        <div ng-switch-when=\"2\" class=\"animate-view-state\">\n" +
+    "        <div ng-switch-when=\"3\" class=\"animate-view-state\">\n" +
     "            <complete-exercise-summary></complete-exercise-summary>\n" +
     "        </div>\n" +
     "    </ng-switch>\n" +
