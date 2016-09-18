9879e9ed4d63e0069da0726778888dab80c5d841
Allen Jacobson <allen@zinkerz.com>
2016-09-18 13:15:19 +0300

    added functionality of opening educator login/signup directly

--- src/components/loginApp/directives/loginApp.directive.js
+++ src/components/loginApp/directives/loginApp.directive.js
@@ -25,10 +25,10 @@
 
                     LoginAppSrv.setSocialProvidersConfig(socialProvidersArr, scope.d.appContext.id);
 
-                    scope.currentUserContext =  'student';
+                    scope.currentUserContext = 'student';
                     scope.currentForm = 'signup';
 
-                    scope.selectApp = function(app) {
+                    scope.selectApp = function (app) {
                         scope.d.appContext = app;
                         LoginAppSrv.setSocialProvidersConfig(socialProvidersArr, scope.d.appContext.id);
                     };
@@ -40,15 +40,15 @@
                     scope.changeUserContext = function (context) {
                         scope.d.userContext = context;
                         if (scope.d.userContext === LoginAppSrv.USER_CONTEXT.STUDENT) {
-                            scope.currentUserContext =  'student';
+                            scope.currentUserContext = 'student';
                         } else if (scope.d.userContext === LoginAppSrv.USER_CONTEXT.TEACHER) {
-                            scope.currentUserContext =  'teacher';
+                            scope.currentUserContext = 'teacher';
                         }
                     };
 
                     // App select menu
                     var originatorEv;
-                    scope.openMenu = function($mdOpenMenu, ev) {
+                    scope.openMenu = function ($mdOpenMenu, ev) {
                         originatorEv = ev;
                         $mdOpenMenu(ev);
                     };
@@ -56,7 +56,7 @@
                     var search = $location.search();
                     if (!angular.equals(search, {}) && (search.app || search.state)) {
                         if (search.app) {
-                            angular.forEach(LoginAppSrv.APPS, function(app, index){
+                            angular.forEach(LoginAppSrv.APPS, function (app, index) {
                                 if (index.toLowerCase() === search.app.toLowerCase()) {
                                     scope.selectApp(app);
                                 }
@@ -64,13 +64,20 @@
                         }
                         if (search.state) {
                             scope.changeCurrentForm(search.state);
+                            if (search.userType) {
+                                if (search.userType === 'educator') {
+                                    scope.changeUserContext(scope.d.userContextObj.TEACHER);
+                                } else {
+                                    scope.changeUserContext(scope.d.userContextObj.STUDENT);
+                                }
+                            }
                         }
                         // $location.search('app', null);
                         // $location.search('state', null);
                     }
 
                     //catching $mdMenuOpen event emitted from angular-material.js
-                    scope.$on('$mdMenuOpen', function() {
+                    scope.$on('$mdMenuOpen', function () {
                         $timeout(function () {
                             //getting menu content container by tag id from html
                             var menuContentContainer = angular.element($document[0].getElementById('app-select-menu'));
