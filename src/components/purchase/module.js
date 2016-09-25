67cdc96c225df2c62992d547f6a8fc1500a98b07
Ofir Nadav <ofir@zinkerz.com>
2016-09-25 17:50:35 +0300

    Implement the purchase mechanism

--- src/components/purchase/module.js
+++ src/components/purchase/module.js
@@ -2,7 +2,8 @@
     'use strict';
 
     angular.module('znk.infra-web-app.purchase',
-        ['ngAnimate',
+        [
+            'ngAnimate',
             'ngMaterial',
             'pascalprecht.translate',
             'znk.infra.svgIcon',
@@ -18,14 +19,14 @@
             function(SvgIconSrvProvider){
 
                 var svgMap = {
-                    'purchase-check-mark': 'components/purchase/svg/check-mark-icon.svg',
-                    'purchase-close-popup': 'components/purchase/svg/close-popup.svg',
+                    'check-mark': 'components/purchase/svg/check-mark-icon.svg',
+                    'close-popup': 'components/purchase/svg/close-popup.svg',
                     'purchase-popup-bullet-1-icon': 'components/purchase/svg/purchase-popup-bullet-1-icon.svg',
                     'purchase-popup-bullet-2-icon': 'components/purchase/svg/purchase-popup-bullet-2-icon.svg',
                     'purchase-popup-bullet-3-icon': 'components/purchase/svg/purchase-popup-bullet-3-icon.svg',
                     'purchase-popup-bullet-4-icon': 'components/purchase/svg/purchase-popup-bullet-4-icon.svg',
                     'purchase-popup-bullet-5-icon': 'components/purchase/svg/purchase-popup-bullet-5-icon.svg',
-                    'purchase-raccoon-logo-icon': 'components/purchase/svg/raccoon-logo.svg'
+                    'raccoon-logo-icon': 'components/purchase/svg/raccoon-logo.svg'
                 };
                 SvgIconSrvProvider.registerSvgSources(svgMap);
             }]);
