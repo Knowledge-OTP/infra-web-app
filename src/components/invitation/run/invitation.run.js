0ba9824d4aa2819c9ab4f69b07112dd6d1efb818
Oded <oded.sharir@gmail.com>
2016-10-05 16:08:56 +0300

    add invitation run

---
+++ src/components/invitation/run/invitation.run.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-web-app.invitation')
+        .run(function($location, InvitationService){
+            'ngInject';
+            var search = $location.search();
+
+            if (angular.isDefined(search['iid'])) {
+                InvitationService.showInvitationConfirm(search.iid);
+                delete search.iid;
+                $location.search(search);
+            }
+        });
+
+})(angular);
