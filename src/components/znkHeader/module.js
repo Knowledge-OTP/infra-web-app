8dd33c176f47c082d8af3083fb9cbbb8226a1733
zinkerz\Ofir Nadav <ofir@zinkerz.com>
2016-10-13 11:12:07 +0300

    znkHeader

--- src/components/znkHeader/module.js
+++ src/components/znkHeader/module.js
@@ -20,7 +20,8 @@
             function(SvgIconSrvProvider){
 
                 var svgMap = {
-                    'znkHeader-raccoon-logo-icon': 'components/znkHeader/svg/raccoon-logo.svg'
+                    'znkHeader-raccoon-logo-icon': 'components/znkHeader/svg/raccoon-logo.svg',
+                    'znkHeader-check-mark-icon': 'components/znkHeader/svg/check-mark-icon.svg'
                 };
                 SvgIconSrvProvider.registerSvgSources(svgMap);
             }]);
