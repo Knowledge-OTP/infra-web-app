194638132e0fdd4eb3de9deb33fac16876fdf6cf
assafhp <assafshp@zahav.net.il>
2016-10-13 19:57:27 +0300

    fixed

--- dist/purchase/purchase.js
+++ dist/purchase/purchase.js
@@ -86,6 +86,11 @@
                         var userId = results[0].auth.uid;
                         var productId = results[1].id;
 
+                        if (!userEmail) {
+                            $log.error('Invalid user attribute: userEmail is not defined, generating uid email');
+                            userEmail = userId + '@zinkerz.com';
+                        }
+
                         if (userEmail && userId) {
                             vm.userEmail = userEmail;
                             vm.hostedButtonId = ENV.purchasePaypalParams.hostedButtonId;
