96d5f64931892717e25bf433e191bd300339e354
Allen Jacobson <allen@zinkerz.com>
2016-10-25 14:47:24 +0300

    firstLoginfix

--- dist/main.js
+++ dist/main.js
@@ -7119,7 +7119,6 @@ angular.module('znk.infra-web-app.liveLessons').run(['$templateCache', function(
                     return globalRef.createUser(formData).then(function () {
                         return LoginAppSrv.login(appContext, userContext, formData).then(function () {
                             isSignUpInProgress = false;
-                            _addFirstRegistrationRecord(appContext, userContext);
                             return _writeUserProfile(formData, appContext, userContext).then(function () {
                                 _redirectToPage(appContext, userContext);
                             });
