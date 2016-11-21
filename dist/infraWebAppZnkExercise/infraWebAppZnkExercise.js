35c62dd40bd2a4c21afa489140a5ce174c08dcfa
odeds <oded.sharir@gmail.com>
2016-11-21 05:00:56 -0800

    Oded/tfl 1412 (#223)
    * change $viewChangeListeners to $watch
    
    * new dist
    
    * change docs
    
    * new dist

--- dist/infraWebAppZnkExercise/infraWebAppZnkExercise.js
+++ dist/infraWebAppZnkExercise/infraWebAppZnkExercise.js
@@ -63,103 +63,111 @@
             SvgIconSrvProvider.registerSvgSources(svgMap);
         }])
         .directive('answerExplanation',
-            ["ZnkExerciseViewModeEnum", "znkAnalyticsSrv", "$timeout", function (ZnkExerciseViewModeEnum, znkAnalyticsSrv, $timeout) {
-                'ngInject';
+        ["ZnkExerciseViewModeEnum", "znkAnalyticsSrv", "$timeout", function (ZnkExerciseViewModeEnum, znkAnalyticsSrv, $timeout) {
+            'ngInject';
 
-                var directive = {
-                    scope: {},
-                    require: ['^questionBuilder', '^ngModel'],
-                    templateUrl: 'components/infraWebAppZnkExercise/directives/answerExplanation/answerExplanation.template.html',
-                    link: function link(scope, element, attrs, ctrls) {
+            var directive = {
+                scope: {},
+                require: ['^questionBuilder', '^ngModel'],
+                templateUrl: 'components/infraWebAppZnkExercise/directives/answerExplanation/answerExplanation.template.html',
+                link: function link(scope, element, attrs, ctrls) {
 
-                        var questionBuilderCtrl = ctrls[0];
-                        var ngModelCtrl = ctrls[1];
-                        var viewMode = questionBuilderCtrl.getViewMode();
-                        var question = questionBuilderCtrl.question;
+                    var questionBuilderCtrl = ctrls[0];
+                    var ngModelCtrl = ctrls[1];
+                    var viewMode = questionBuilderCtrl.getViewMode();
+                    var question = questionBuilderCtrl.question;
 
-                        scope.d = {};
+                    scope.d = {};
 
-                        var init = (function () {
-                            var wasInit;
+                    var init = (function () {
+                        var wasInit;
 
-                            return function () {
-                                if (wasInit) {
-                                    return;
-                                }
-
-                                // add timeout to prevent showing visible answer explanation for a
-                                // second before it's hidden on slide that is not the current slide
-                                // (because the slider shifts from first slide to current)
-                                $timeout(function () {
-                                    element.addClass('answer-explanation-visible');
-                                }, 0, false);
-
-                                var analyticsProps = {
-                                    subjectType: question.subjectId,
-                                    questionId: question.id
-                                };
-
-                                scope.$watch('d.showWrittenSln', function (isVisible) {
-                                    if (isVisible || isVisible === false) {
-                                        if (isVisible) {
-                                            znkAnalyticsSrv.eventTrack({
-                                                eventName: 'writtenSolutionClicked',
-                                                props: analyticsProps
-                                            });
-                                            znkAnalyticsSrv.timeTrack({eventName: 'writtenSolutionClosed'});
-                                        } else {
-                                            znkAnalyticsSrv.eventTrack({
-                                                eventName: 'writtenSolutionClosed',
-                                                props: analyticsProps
-                                            });
-                                        }
-                                    }
-                                });
-
-                                wasInit = true;
-                            };
-                        })();
-
-                        function viewChangeListener() {
-                            if (ngModelCtrl.$viewValue) {           // user already answered
-                                init();
-                            } else {
-                                ngModelCtrl.$viewChangeListeners.push(function () {
-                                    init();
-                                });
+                        return function () {
+                            if (wasInit) {
+                                return;
                             }
-                        }
 
-                        switch (viewMode) {
-                            case ZnkExerciseViewModeEnum.REVIEW.enum:
-                                init();
-                                break;
-                            case ZnkExerciseViewModeEnum.ANSWER_WITH_RESULT.enum:
-                                viewChangeListener();
-                                break;
-                        }
+                            // add timeout to prevent showing visible answer explanation for a
+                            // second before it's hidden on slide that is not the current slide
+                            // (because the slider shifts from first slide to current)
+                            $timeout(function () {
+                                element.addClass('answer-explanation-visible');
+                            }, 0, false);
 
-                        function _updateBindExercise() {
-                            questionBuilderCtrl.bindExerciseEventManager.update('answerExplanation', { data: scope.d.toggleWrittenSln, update: true }, question.id);
-                        }
+                            var analyticsProps = {
+                                subjectType: question.subjectId,
+                                questionId: question.id
+                            };
 
-                        scope.d.close = function () {
-                            scope.d.toggleWrittenSln = false;
-                            _updateBindExercise();
+                            scope.$watch('d.showWrittenSln', function (isVisible) {
+                                if (isVisible || isVisible === false) {
+                                    if (isVisible) {
+                                        znkAnalyticsSrv.eventTrack({
+                                            eventName: 'writtenSolutionClicked',
+                                            props: analyticsProps
+                                        });
+                                        znkAnalyticsSrv.timeTrack({ eventName: 'writtenSolutionClosed' });
+                                    } else {
+                                        znkAnalyticsSrv.eventTrack({
+                                            eventName: 'writtenSolutionClosed',
+                                            props: analyticsProps
+                                        });
+                                    }
+                                }
+                            });
+
+                            wasInit = true;
                         };
+                    })();
 
-                        scope.d.toggleAnswer = function () {
-                            scope.d.toggleWrittenSln = !scope.d.toggleWrittenSln;
-                            _updateBindExercise();
-                        };
-
-                        questionBuilderCtrl.bindExerciseEventManager.registerCb('answerExplanation', function (newVal) {
-                            scope.d.toggleWrittenSln = newVal.data;
-                        }, question.id);
+                    function viewChangeListener() {
+                        if (ngModelCtrl.$viewValue) {           // user already answered
+                            init();
+                        } else {
+                            // $watch seems to work for sharer and viewer, while $viewChangeListeners
+                            // worked only for sharer. it's because $viewChangeListeners does not  
+                            // invoke when the $modalValue change, only when the $viewValue (via input and etc)
+                            scope.$watch(function () {
+                                return ngModelCtrl.$viewValue;
+                            }, function (newVal) {
+                                // newVal undefined meens no answer yet, so must be protected 
+                                if (angular.isDefined(newVal)) {
+                                    init();
+                                }
+                            });
+                        }
                     }
-                };
-                return directive;
-            }]
+
+                    switch (viewMode) {
+                        case ZnkExerciseViewModeEnum.REVIEW.enum:
+                            init();
+                            break;
+                        case ZnkExerciseViewModeEnum.ANSWER_WITH_RESULT.enum:
+                            viewChangeListener();
+                            break;
+                    }
+
+                    function _updateBindExercise() {
+                        questionBuilderCtrl.bindExerciseEventManager.update('answerExplanation', { data: scope.d.toggleWrittenSln, update: true }, question.id);
+                    }
+
+                    scope.d.close = function () {
+                        scope.d.toggleWrittenSln = false;
+                        _updateBindExercise();
+                    };
+
+                    scope.d.toggleAnswer = function () {
+                        scope.d.toggleWrittenSln = !scope.d.toggleWrittenSln;
+                        _updateBindExercise();
+                    };
+
+                    questionBuilderCtrl.bindExerciseEventManager.registerCb('answerExplanation', function (newVal) {
+                        scope.d.toggleWrittenSln = newVal.data;
+                    }, question.id);
+                }
+            };
+            return directive;
+        }]
         );
 })(angular);
 
