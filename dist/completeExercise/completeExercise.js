72aa93b0ccfd58ab0a2cbe8871bc1494d611512f
Oren Or <orenor10@barak.net.il>
2016-11-27 16:44:28 +0200

    Oren/sat sprint 8 (#233)
    * invitations service changes
    
    * invitations service changes
    
    * invitations service changes
    
    * invitations service changes
    
    * invitations service changes
    
    * invitations service changes

--- dist/completeExercise/completeExercise.js
+++ dist/completeExercise/completeExercise.js
@@ -664,7 +664,7 @@
 
                 exerciseResult.subjectId = exerciseContent.subjectId;
                 exerciseResult.exerciseName = exerciseContent.name;
-                exerciseResult.totalQuestionNum = exerciseContent.questions.length;
+                exerciseResult.totalQuestionNum = (exerciseTypeId === ExerciseTypeEnum.LECTURE.enum ? 0 : exerciseContent.questions.length);
                 exerciseResult.calculator = exerciseContent.calculator;
                 exerciseResult.timePreference = exerciseContent.timePreference;
                 exerciseResult.categoryId = exerciseContent.categoryId;
