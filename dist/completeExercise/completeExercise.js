50adec2e315f80733f9efa3c5a8dec10d0aa3b61
odeds <oded.sharir@gmail.com>
2016-12-05 08:37:57 -0800

    Oded/dshb 825 sharing (#253)
    * fixed bug
    
    * new dist

--- dist/completeExercise/completeExercise.js
+++ dist/completeExercise/completeExercise.js
@@ -265,7 +265,7 @@
                             if (isDiffActiveScreen) {
                                 var newViewState = activeExercise.activeScreen || VIEW_STATES.NONE;
                                 //active screen should never be none if in sharer mode
-                                if (!(newViewState === VIEW_STATES.NONE && isSharerMode)) {
+                                if (newViewState === VIEW_STATES.NONE && isSharerMode) {
                                     $ctrl.changeViewState(newViewState, true);
                                 }
                             }
