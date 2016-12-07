fd7a3e60511706a9e35faa448bc93590c5d4f84f
odeds <oded.sharir@gmail.com>
2016-12-07 03:07:01 -0800

    Oded/dshb 839 (#259)
    * fix bug
    
    * new dist
    
    * new dist

--- src/components/completeExercise/directives/completeExercise/completeExercise.directive.js
+++ src/components/completeExercise/directives/completeExercise/completeExercise.directive.js
@@ -250,10 +250,13 @@
                         if (isSameExerciseId && isSameExerciseType) {
                             if (isDiffActiveScreen) {
                                 var newViewState = activeExercise.activeScreen || VIEW_STATES.NONE;
+
                                 //active screen should never be none if in sharer mode
                                 if (newViewState === VIEW_STATES.NONE && isSharerMode) {
-                                    $ctrl.changeViewState(newViewState, true);
+                                    newViewState = VIEW_STATES.EXERCISE;
                                 }
+
+                                $ctrl.changeViewState(newViewState, true);
                             }
                         } else {
                             if (isViewerMode) {
