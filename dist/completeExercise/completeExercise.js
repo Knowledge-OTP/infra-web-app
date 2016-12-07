1e019cce69e134805ee9470bc1b5272bab00c59b
Allensy <allensy89@gmail.com>
2016-12-07 13:06:48 +0200

    save work (#257)

--- dist/completeExercise/completeExercise.js
+++ dist/completeExercise/completeExercise.js
@@ -700,6 +700,8 @@
                 exerciseResult.moduleId = exerciseContent.moduleId;
                 exerciseResult.time = exerciseContent.time;
                 exerciseResult.exerciseOrder = settings.exerciseDetails.exerciseOrder;
+                exerciseResult.parentTypeId = exerciseContent.parentTypeId;
+
 
                 if (angular.isUndefined(exerciseResult.startedTime)) {
                     exerciseResult.startedTime = Date.now();
--- dist/main.js
+++ dist/main.js
@@ -752,6 +752,8 @@ angular.module('znk.infra-web-app.angularMaterialOverride').run(['$templateCache
                 exerciseResult.moduleId = exerciseContent.moduleId;
                 exerciseResult.time = exerciseContent.time;
                 exerciseResult.exerciseOrder = settings.exerciseDetails.exerciseOrder;
+                exerciseResult.parentTypeId = exerciseContent.parentTypeId;
+
 
                 if (angular.isUndefined(exerciseResult.startedTime)) {
                     exerciseResult.startedTime = Date.now();
