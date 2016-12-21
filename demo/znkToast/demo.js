bb434e4642841bd29924a300d784ce6c8bf47f31
Oded <oded.sharir@gmail.com>
2016-12-21 11:37:43 +0200

    add html instead of text to znk toast

--- demo/znkToast/demo.js
+++ demo/znkToast/demo.js
@@ -19,7 +19,7 @@
 
             $scope.openToastProgress = function() {
                 var type = 'progress';
-                var msg = 'progress progress progress progress';
+                var msg = '<span class="progress-title">Processing your recording</span> <span class="progress-content">Do not close the Zinkerz app</span>';
                 ZnkToastSrv.showToast(type, msg, {
                     hideDelay: false
                 });
