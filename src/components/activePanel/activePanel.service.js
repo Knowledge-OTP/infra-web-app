fd0cb4697ef0dbbb807a9189b0c7e025bbd99719
Ofir Nadav <ofir.nadav@gmail.com>
2016-12-22 19:38:16 +0200

    liveSession

---
+++ src/components/activePanel/activePanel.service.js
@@ -0,0 +1,24 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-web-app.activePanel').service('ActivePanelSrv',
+        function ($document, $compile, $rootScope) {
+            'ngInject';
+
+            var self = this;
+
+            this.loadActivePanel = function () {
+                var body = angular.element($document).find('body');
+
+                var canvasContainerElement = angular.element(
+                    '<active-panel></active-panel>'
+                );
+
+                if (!angular.element(body[0].querySelector('active-panel')).length) {
+                    self.scope = $rootScope.$new(true);
+                    body.append(canvasContainerElement);
+                    $compile(canvasContainerElement.contents())(self.scope);
+                }
+            };
+        });
+})(angular);
---
+++ src/components/liveSession/module.js
@@ -0,0 +1,33 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-web-app.liveSession',
+        [
+            'ngMaterial',
+            'znk.infra.popUp',
+            'pascalprecht.translate',
+            'znk.infra.auth',
+            'znk.infra.userContext',
+            'znk.infra.utility',
+            'znk.infra.analytics',
+            'znk.infra.general',
+            'znk.infra.user',
+            'znk.infra.svgIcon',
+            'znk.infra.mailSender',
+            'znk.infra.exerciseUtility',
+            'znk.infra.calls',
+            'znk.infra-web-app.activePanel',
+            'znk.infra-web-app.znkToast'
+        ])
+        .config([
+            'SvgIconSrvProvider',
+            function (SvgIconSrvProvider) {
+                var svgMap = {
+                    'liveSession-english-icon': 'components/liveSession/svg/liveSession-verbal-icon.svg',
+                    'liveSession-math-icon': 'components/liveSession/svg/liveSession-math-icon.svg',
+                    'liveSession-start-lesson-popup-icon': 'components/liveSession/svg/liveSession-start-lesson-popup-icon.svg'
+                };
+                SvgIconSrvProvider.registerSvgSources(svgMap);
+            }
+        ]);
+})(angular);
