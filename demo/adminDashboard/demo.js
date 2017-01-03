fee9b55b3a3b779b07712d2b177910255355c461
Alex <choroshin@gmail.com>
2017-01-03 20:21:08 +0200

    fix auth for adminDashboard demo

--- demo/adminDashboard/demo.js
+++ demo/adminDashboard/demo.js
@@ -1,6 +1,17 @@
 (function (angular) {
 
-    angular.module('demo', ['znk.infra-web-app.adminDashboard'])
+    angular.module('demo', [
+        'znk.infra-web-app.adminDashboard',
+        'demoEnv',
+        'znk.infra.userContext',
+        'znk.infra.config',
+        'znk.infra.user',
+        'ngMaterial',
+        'znk.infra.auth',
+        'znk.infra.presence',
+        'znk.infra.analytics',
+        'pascalprecht.translate'
+    ])
         .config([
             '$stateProvider',
             function ($stateProvider) {
@@ -16,24 +27,70 @@
                         controllerAs: 'vm'
                     });
             }
+
         ])
-        .constant('ENV', {
-            firebaseAppScopeName: "act_dashboard",
-            fbGlobalEndPoint: "https://znk-dev.firebaseio.com/",
-            fbDataEndPoint: "https://act-dev.firebaseio.com/",
-            appContext: 'student',
-            studentAppName: 'sat_app',
-            dashboardAppName: 'sat_dashboard',
-            videosEndPoint: "//dfz02hjbsqn5e.cloudfront.net/sat_app/",
-            mediaEndPoint: "//dfz02hjbsqn5e.cloudfront.net/",
-            backendEndpoint: "https://znk-web-backend-dev.azurewebsites.net/",
-            teachworksDataUrl: 'teachworks',
-            debug: true,
-            "elasticSearch": {
+        .config(function () {
+            // Replace storageConfig parameters through localStorage
+            localStorage.setItem('email', 'alex+100@zinkerz.com');
+            localStorage.setItem('password', '9917364');
+            localStorage.setItem('dataDbPath', 'https://act-dev.firebaseio.com/');
+            localStorage.setItem('studentPath', '/act_app');
+            localStorage.setItem('teacherPath', '/act_dashboard');
+        })
+        .config(function (PresenceServiceProvider, znkAnalyticsSrvProvider) {
+
+            PresenceServiceProvider.setAuthServiceName('AuthService');
+            znkAnalyticsSrvProvider.setEventsHandler(function () {
+                return {
+                    eventTrack: angular.noop,
+                    timeTrack: angular.noop,
+                    pageTrack: angular.noop,
+                    setUsername: angular.noop,
+                    setUserProperties: angular.noop
+                };
+            });
+        })
+        .decorator('ENV', function ($delegate) {
+            'ngInject';
+
+            $delegate.firebaseAppScopeName = 'act_dashboard';
+            $delegate.fbDataEndPoint = '//act-dev.firebaseio.com/';
+            $delegate.appContext = 'dashboard';
+            $delegate.studentAppName = 'act_app';
+            $delegate.dashboardAppName = 'act_dashboard';
+            $delegate.videosEndPoint = '//dfz02hjbsqn5e.cloudfront.net/act_app/';
+            $delegate.mediaEndPoint = '//dfz02hjbsqn5e.cloudfront.net/';
+            $delegate.fbGlobalEndPoint = '//znk-dev.firebaseio.com/';
+            $delegate.backendEndpoint = '//znk-web-backend-dev.azurewebsites.net/';
+            $delegate.teachworksDataUrl = 'teachworks';
+            $delegate.userIdleTime = 30;
+            $delegate.idleTimeout = 0;
+            $delegate.idleKeepalive = 2;
+            $delegate.plivoUsername = "ZinkerzDev160731091034";
+            $delegate.plivoPassword = "zinkerz$9999";
+            $delegate.elasticSearch = {
                 "host": "znk-elastic-dev4891.cloudapp.net:9200",
                 "apiVersion": "5.x",
                 "log": "trace"
             }
+
+            return $delegate;
+        })
+        .decorator('StudentContextSrv', function ($delegate) {
+            'ngInject';
+
+            $delegate.getCurrUid = function () {
+                return 'e6a83840-01cc-4e15-af9b-26d9dbbd87d7';
+            };
+            return $delegate;
+        })
+        .decorator('AuthService', function ($delegate) {
+            'ngInject';
+
+            $delegate.getAuth = function () {
+                return {uid: 'cf656635-b44c-4fcc-82ef-72fe566d5540'};
+            };
+            return $delegate;
         })
         .controller('Main', function ($state) {
             var self = this;
