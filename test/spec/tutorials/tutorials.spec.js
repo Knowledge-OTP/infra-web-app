0d034698ce572a6ded66edd12cc1cc2be6a52700
Allen Jacobson <allen@zinkerz.com>
2017-01-15 18:51:54 +0200

    save work

---
+++ test/spec/tutorials/tutorials.spec.js
@@ -0,0 +1,9 @@
+describe('testing service "TutorialsSrv"', function(){
+    'use strict';
+    
+    beforeEach(module('znk.infra-web-app.tutorials', 'htmlTemplates', 'storage.mock', 'testUtility', 'user.mock', 'env.mock'));
+
+    beforeEach(module('znk.infra-web-app.tutorials', function(TutorialsSrv) {
+
+    }))
+})
