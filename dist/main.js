4a03f9933f5b24002c5785ff3e11f7e014e09b69
Alex <choroshin@gmail.com>
2017-01-16 15:47:21 +0200

    save

--- dist/main.js
+++ dist/main.js
@@ -1199,45 +1199,6 @@ angular.module('znk.infra-web-app.adminDashboard').run(['$templateCache', functi
     "</g>\n" +
     "</svg>\n" +
     "");
-  $templateCache.put("components/adminDashboard/components/eMetadata/templates/eMetadata.template.html",
-    "<div class=\"admin-dashboard admin-eMetadata\" translate-namespace=\"ADMIN\">\n" +
-    "\n" +
-    "    <div class=\"admin-main-container-overlay\">\n" +
-    "        <div class=\"admin-search-container\">\n" +
-    "            <div class=\"admin-search-label\" translate=\"ADMIN.ESLINK.SEARCH_EDUCATOR\"></div>\n" +
-    "            <div class=\"admin-search-pane\">\n" +
-    "                <div class=\"search-wrap\">\n" +
-    "                    <div class=\"znk-input-group\">\n" +
-    "                        <input type=\"search\"\n" +
-    "                               minlength=\"3\"\n" +
-    "                               placeholder=\"{{'ADMIN.ESLINK.SEARCH_EDUCATOR' | translate}}\"\n" +
-    "                               name=\"search-box\"\n" +
-    "                               ng-model=\"vm.educatorSearchQuery\">\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <button class=\"admin-search-btn\" ng-click=\"vm.getEducatorsSearchResults(vm.educatorSearchQuery)\"\n" +
-    "                        ng-disabled=\"!vm.educatorSearchQuery\" translate=\".SEARCH\">\n" +
-    "                </button>\n" +
-    "\n" +
-    "            </div>\n" +
-    "            <div class=\"admin-search-msg\" translate=\"ADMIN.MIN_SEARCH_LENGTH\"></div>\n" +
-    "            <div   ui-grid-selection ui-grid=\"vm.gridEducatorsOptions\" class=\"admin-grid\" >\n" +
-    "                <div class=\"admin-ui-grid-msg\" ng-if=\"vm.uiGridState.educator.initial\">\n" +
-    "                    <div class=\"admin-msg\">\n" +
-    "                        <div translate=\"ADMIN.ESLINK.EDUCATOR_INITIAL_MSG\"></div>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <div class=\"admin-ui-grid-msg\" ng-if=\"vm.uiGridState.educator.noData\">\n" +
-    "                    <div class=\"admin-msg\">\n" +
-    "                        <span>{{'ADMIN.ESLINK.EDUCATOR_NODATA_MSG' | translate}} '{{vm.educatorSearchNoData}}'</span>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "</div>\n" +
-    "");
   $templateCache.put("components/adminDashboard/components/eMetadata/templates/educatorProfile.template.html",
     "<md-dialog ng-cloak class=\"admin-dashboard admin-profile\" translate-namespace=\"ADMIN\">\n" +
     "    <div class=\"top-icon-wrap\">\n" +
@@ -1394,6 +1355,45 @@ angular.module('znk.infra-web-app.adminDashboard').run(['$templateCache', functi
     "\n" +
     "\n" +
     "\n" +
+    "");
+  $templateCache.put("components/adminDashboard/components/eMetadata/templates/eMetadata.template.html",
+    "<div class=\"admin-dashboard admin-eMetadata\" translate-namespace=\"ADMIN\">\n" +
+    "\n" +
+    "    <div class=\"admin-main-container-overlay\">\n" +
+    "        <div class=\"admin-search-container\">\n" +
+    "            <div class=\"admin-search-label\" translate=\"ADMIN.ESLINK.SEARCH_EDUCATOR\"></div>\n" +
+    "            <div class=\"admin-search-pane\">\n" +
+    "                <div class=\"search-wrap\">\n" +
+    "                    <div class=\"znk-input-group\">\n" +
+    "                        <input type=\"search\"\n" +
+    "                               minlength=\"3\"\n" +
+    "                               placeholder=\"{{'ADMIN.ESLINK.SEARCH_EDUCATOR' | translate}}\"\n" +
+    "                               name=\"search-box\"\n" +
+    "                               ng-model=\"vm.educatorSearchQuery\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "                <button class=\"admin-search-btn\" ng-click=\"vm.getEducatorsSearchResults(vm.educatorSearchQuery)\"\n" +
+    "                        ng-disabled=\"!vm.educatorSearchQuery\" translate=\".SEARCH\">\n" +
+    "                </button>\n" +
+    "\n" +
+    "            </div>\n" +
+    "            <div class=\"admin-search-msg\" translate=\"ADMIN.MIN_SEARCH_LENGTH\"></div>\n" +
+    "            <div   ui-grid-selection ui-grid=\"vm.gridEducatorsOptions\" class=\"admin-grid\" >\n" +
+    "                <div class=\"admin-ui-grid-msg\" ng-if=\"vm.uiGridState.educator.initial\">\n" +
+    "                    <div class=\"admin-msg\">\n" +
+    "                        <div translate=\"ADMIN.ESLINK.EDUCATOR_INITIAL_MSG\"></div>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "                <div class=\"admin-ui-grid-msg\" ng-if=\"vm.uiGridState.educator.noData\">\n" +
+    "                    <div class=\"admin-msg\">\n" +
+    "                        <span>{{'ADMIN.ESLINK.EDUCATOR_NODATA_MSG' | translate}} '{{vm.educatorSearchNoData}}'</span>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>\n" +
     "");
   $templateCache.put("components/adminDashboard/components/esLink/directives/app-select.template.html",
     "<div class=\"znk-app-select\">\n" +
@@ -4760,17 +4760,29 @@ angular.module('znk.infra-web-app.diagnosticIntro').run(['$templateCache', funct
 
     angular.module('znk.infra-web-app.elasticSearch')
         .service('ElasticSearchSrv',
-            ["ENV", "$log", "$http", function (ENV,$log,$http) {
+            ["ENV", "$log", "$http", "AuthService", function (ENV, $log, $http, AuthService) {
                 'ngInject';
+                var uidObj = AuthService.getAuth();
 
-                var apiPath = ENV.backendEndpoint + "/search";
-
+              //  var apiPath = ENV.backendEndpoint + "/search";
+                var apiPath ="http://localhost:3009/search";
                 this.search = function (query) {
+                    var uid =uidObj.uid;
+
+                    if (!angular.isString(uid)) {
+                        $log.error('ElasticSearchSrv: uid is not a string or not exist');
+                        return;
+                    }
                     if (!query && !angular.isObject(query)) {
                         $log.error('ElasticSearchSrv: query is empty or not an object');
                         return;
                     }
-                    return $http.post(apiPath, query);
+                    var searchObj = {
+                        query: query,
+                        uid: uid,
+                        appName: ENV.firebaseAppScopeName
+                    };
+                    return $http.post(apiPath, searchObj);
                 };
             }]
         );
@@ -12278,21 +12290,6 @@ angular.module('znk.infra-web-app.onBoarding').run(['$templateCache', function($
 (function (angular) {
     'use strict';
 
-    angular.module('znk.infra-web-app.promoCode').service('PromoCodeTypeEnum',['EnumSrv',
-        function(EnumSrv) {
-
-            var PromoCodeTypeEnum = new EnumSrv.BaseEnum([
-                ['FREE_LICENSE', 1, 'free license'],
-                ['ZINKERZ_EDUCATOR', 2, 'zinkerz educator'],
-            ]);
-
-            return PromoCodeTypeEnum;
-        }]);
-})(angular);
-
-(function (angular) {
-    'use strict';
-
     angular.module('znk.infra-web-app.promoCode').constant('PROMO_CODE_STATUS', {
         accepted: 0,
         invalid: 1
@@ -12391,6 +12388,21 @@ angular.module('znk.infra-web-app.onBoarding').run(['$templateCache', function($
             }];
         }
     );
+})(angular);
+
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-web-app.promoCode').service('PromoCodeTypeEnum',['EnumSrv',
+        function(EnumSrv) {
+
+            var PromoCodeTypeEnum = new EnumSrv.BaseEnum([
+                ['FREE_LICENSE', 1, 'free license'],
+                ['ZINKERZ_EDUCATOR', 2, 'zinkerz educator'],
+            ]);
+
+            return PromoCodeTypeEnum;
+        }]);
 })(angular);
 
 angular.module('znk.infra-web-app.promoCode').run(['$templateCache', function($templateCache) {
@@ -14068,6 +14080,19 @@ angular.module('znk.infra-web-app.tutorials').component('tutorialPane', {
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra-web-app.tutorials').controller('TutorialsRoadmapController',
+        ["$translatePartialLoader", "tutorials", function ($translatePartialLoader, tutorials) {
+            'ngInject';
+            $translatePartialLoader.addPart('tutorials');
+            var vm = this;
+            vm.tutorials = tutorials;
+        }]
+    );
+})(angular);
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra-web-app.tutorials').controller('TutorialWorkoutController',
         ["exerciseData", function(exerciseData) {
             'ngInject';
@@ -14080,19 +14105,6 @@ angular.module('znk.infra-web-app.tutorials').component('tutorialPane', {
         this.completeExerciseSettings = {
             exitAction: exerciseData.exitAction
         };
-        }]
-    );
-})(angular);
-
-(function (angular) {
-    'use strict';
-
-    angular.module('znk.infra-web-app.tutorials').controller('TutorialsRoadmapController',
-        ["$translatePartialLoader", "tutorials", function ($translatePartialLoader, tutorials) {
-            'ngInject';
-            $translatePartialLoader.addPart('tutorials');
-            var vm = this;
-            vm.tutorials = tutorials;
         }]
     );
 })(angular);
@@ -14247,18 +14259,18 @@ angular.module('znk.infra-web-app.tutorials').run(['$templateCache', function($t
     "    </g>\n" +
     "</svg>\n" +
     "");
-  $templateCache.put("components/tutorials/templates/tutorialWorkout.template.html",
-    "<div class=\"complete-exercise-container base-border-radius\">\n" +
-    "    <complete-exercise exercise-details=\"vm.completeExerciseDetails\"\n" +
-    "                       settings=\"vm.completeExerciseSettings\">\n" +
-    "    </complete-exercise>\n" +
-    "</div>");
   $templateCache.put("components/tutorials/templates/tutorialsRoadmap.template.html",
     "<div class=\"tutorials-main-container\">\n" +
     "    <tutorial-pane ng-model=\"vm.activeSubject\"></tutorial-pane>\n" +
     "    <tutorial-list ng-model=\"vm.activeSubject\" tutorials=\"vm.tutorials\"></tutorial-list>\n" +
     "</div>\n" +
     "");
+  $templateCache.put("components/tutorials/templates/tutorialWorkout.template.html",
+    "<div class=\"complete-exercise-container base-border-radius\">\n" +
+    "    <complete-exercise exercise-details=\"vm.completeExerciseDetails\"\n" +
+    "                       settings=\"vm.completeExerciseSettings\">\n" +
+    "    </complete-exercise>\n" +
+    "</div>");
 }]);
 
 (function (angular) {
--- src/components/elasticSearch/services/elasticsearch.service.js
+++ src/components/elasticSearch/services/elasticsearch.service.js
@@ -3,17 +3,29 @@
 
     angular.module('znk.infra-web-app.elasticSearch')
         .service('ElasticSearchSrv',
-            function (ENV,$log,$http) {
+            function (ENV, $log, $http, AuthService) {
                 'ngInject';
+                var uidObj = AuthService.getAuth();
 
-                var apiPath = ENV.backendEndpoint + "/search";
-
+              //  var apiPath = ENV.backendEndpoint + "/search";
+                var apiPath ="http://localhost:3009/search";
                 this.search = function (query) {
+                    var uid =uidObj.uid;
+
+                    if (!angular.isString(uid)) {
+                        $log.error('ElasticSearchSrv: uid is not a string or not exist');
+                        return;
+                    }
                     if (!query && !angular.isObject(query)) {
                         $log.error('ElasticSearchSrv: query is empty or not an object');
                         return;
                     }
-                    return $http.post(apiPath, query);
+                    var searchObj = {
+                        query: query,
+                        uid: uid,
+                        appName: ENV.firebaseAppScopeName
+                    };
+                    return $http.post(apiPath, searchObj);
                 };
             }
         );
