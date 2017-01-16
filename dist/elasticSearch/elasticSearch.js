dd692d249467b8a133cab0dc8664bded2c42371f
Alex Choroshin <choroshin@gmail.com>
2017-01-16 16:15:58 +0200

    Alex/elastic from backend (#315)
    * save
    
    * save
    
    * save
    
    * save
    
    * dist

--- dist/elasticSearch/elasticSearch.js
+++ dist/elasticSearch/elasticSearch.js
@@ -9,17 +9,29 @@
 
     angular.module('znk.infra-web-app.elasticSearch')
         .service('ElasticSearchSrv',
-            ["ENV", "$log", "$http", function (ENV,$log,$http) {
+            ["ENV", "$log", "$http", "AuthService", function (ENV, $log, $http, AuthService) {
                 'ngInject';
+                var uidObj = AuthService.getAuth();
 
                 var apiPath = ENV.backendEndpoint + "/search";
 
                 this.search = function (query) {
+                    var uid =uidObj.uid;
+
+                    if (!angular.isString(uid)) {
+                        $log.error('ElasticSearchSrv: uid is not a string or not exist');
+                        return;
+                    }
                     if (!query && !angular.isObject(query)) {
                         $log.error('ElasticSearchSrv: query is empty or not an object');
                         return;
                     }
-                    return $http.post(apiPath, query);
+                    var searchObj = {
+                        query: query,
+                        uid: uid,
+                        appName: ENV.firebaseAppScopeName
+                    };
+                    return $http.post(apiPath, searchObj);
                 };
             }]
         );
