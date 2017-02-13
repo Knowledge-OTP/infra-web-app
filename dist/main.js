9cf8ddd175ccfd0e53bd3e5d16665b80bc30d4f7
Alex <choroshin@gmail.com>
2017-02-13 12:04:37 +0200

    save

--- dist/main.js
+++ dist/main.js
@@ -12122,7 +12122,7 @@ angular.module('znk.infra-web-app.myProfile').run(['$templateCache', function($t
                     OnBoardingService.setOnBoardingStep(OnBoardingService.steps.DIAGNOSTIC);
                     $state.go('app.onBoarding.diagnostic');
                 },
-                onExit: function () {
+                exitAction: function () {
                     OnBoardingService.setOnBoardingStep(OnBoardingService.steps.DIAGNOSTIC);
                 }
             };
