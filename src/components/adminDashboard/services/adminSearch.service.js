7c9927de5c82c08957aa270879266cff9ba9e11a
Alex <choroshin@gmail.com>
2017-03-20 19:50:22 +0200

    save

--- src/components/adminDashboard/services/adminSearch.service.js
+++ src/components/adminDashboard/services/adminSearch.service.js
@@ -3,10 +3,12 @@
 
     angular.module('znk.infra-web-app.adminDashboard')
         .service('AdminSearchService',
-            function ($mdDialog, $http, ENV, UserProfileService, $q, $log, ElasticSearchSrv) {
+            function ($mdDialog, $http, ENV, UserProfileService, $q, $log, ElasticSearchSrv, StorageSrv, InfraConfigSrv) {
                 'ngInject';
 
                 var sizeLimit = 10000;
+                var PROMO_CODES_PATH = StorageSrv.variables.appUserSpacePath + '/promoCodes';
+                var hasUB;
 
                 this.getSearchResultsByTerm = function (queryTerm) {
                     return _getSearchResults(queryTerm, _buildQueryBodyByTerm);
@@ -33,13 +35,19 @@
                             "size": sizeLimit
                         }
                     };
-                    buildQuery.call(null, query.body, _makeTerm(queryTerm.toLowerCase()));
-                    ElasticSearchSrv.search(query).then(function (response) {
-                        deferred.resolve(_searchResults(response.data.hits));
+                    hasUBPromoCode().then(function (hasUB) {
+                        buildQuery.call(null, query.body, _makeTerm(queryTerm.toLowerCase()), hasUB);
+                        ElasticSearchSrv.search(query).then(function (response) {
+                            deferred.resolve(_searchResults(response.data.hits));
+                        }, function (err) {
+                            $log.error(err.message);
+                            deferred.reject(err.message);
+                        });
                     }, function (err) {
                         $log.error(err.message);
                         deferred.reject(err.message);
                     });
+
                     return deferred.promise;
                 }
 
@@ -60,7 +68,7 @@
                     return mappedData;
                 }
 
-                function _buildQueryBody(body, term) {
+                function _buildQueryBody(body, term, proo) {
                     body.query = {
                         "query_string": {
                             "fields": ["zinkerzTeacher", "nickname", "email"],
@@ -101,7 +109,21 @@
                     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
                 }
 
-
+                function hasUBPromoCode() {
+                    return InfraConfigSrv.getTeacherStorage().then(function (TeacherStorageSrv) {
+                        return TeacherStorageSrv.get(PROMO_CODES_PATH).then(function (promoCodeData) {
+                            var hasUB = false;
+                            if (promoCodeData) {
+                                data = Object.keys(promoCodeData).filter(function (key) {
+                                    return promoCodeData[key] === ENV.UB;
+                                });
+                            }
+                            return $q.when(hasUB);
+                        });
+                    }).catch(function (err) {
+                        $log.debug('AdminSearchService - getPromoCodes: failed to get getTeacherStorage', err);
+                    });
+                }
             }
         );
 })(angular);
