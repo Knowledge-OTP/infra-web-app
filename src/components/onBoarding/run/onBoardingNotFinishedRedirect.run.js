b2a981f3d40bda6525cb75372b05ccaf4d7157d6
choroshin <choroshin@gmail.com>
2018-06-05 04:12:13 +0300

    save

--- src/components/onBoarding/run/onBoardingNotFinishedRedirect.run.js
+++ src/components/onBoarding/run/onBoardingNotFinishedRedirect.run.js
@@ -6,60 +6,67 @@
         var isOnBoardingCompleted = false;
         $rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState) {//eslint-disable-line
             if (isOnBoardingCompleted) {
-                return;
-            }
-
-            var APP_WORKOUTS_STATE = 'app.workouts.roadmap';
-            var isGoingToWorkoutsState = toState.name.indexOf(APP_WORKOUTS_STATE) !== -1;
-
-            if (isGoingToWorkoutsState) {
-                evt.preventDefault();
-
-                OnBoardingService.isOnBoardingCompleted().then(function (_isOnBoardingCompleted) {
-                    isOnBoardingCompleted = _isOnBoardingCompleted;
-
-                    if (!isOnBoardingCompleted) {
-                        var ON_BOARDING_STATE_NAME = 'app.onBoarding';
-                        var isNotFromOnBoardingState = fromState.name.indexOf(ON_BOARDING_STATE_NAME) === -1;
-                        if (isNotFromOnBoardingState) {
-                            $state.go(ON_BOARDING_STATE_NAME);
-                        }
-                    } else {
-                        OnBoardingService.getMarketingToefl().then(function (marketingObj) {
-                            // 7 - app status
-                            if (marketingObj && marketingObj.status && marketingObj.status !== 7) {
-                                var state = '';
-                                switch (marketingObj.status) {
-                                    case 2:
-                                        state = 'email';
-                                        break;
-                                    case 3:
-                                        state = 'verifyEmail';
-                                        break;
-                                    case 4:
-                                        state = 'purchase';
-                                        break;
-                                    case 5:
-                                        state = 'purchase';
-                                        break;
-                                    case 6:
-                                        state = 'signup';
-                                        break;
-                                    default:
-                                        state = 'purchase';
-                                }
-                                window.location.href = `${ENV.zinkerzWebsiteBaseUrl}myzinkerz/toefl/${state}`;
-                            } else {
-                                $state.go(toState, toParams, {
-                                    reload: true
-                                });
-                            }
-                        });
-                        $state.go(toState, toParams, {
-                            reload: true
-                        });
+                OnBoardingService.getMarketingToefl().then(function (marketingObj) {
+                    // statuses:  7 - app  , 1 - diagnostic
+                    if (marketingObj && marketingObj.status && marketingObj.status !== 1 && marketingObj.status !== 7) {
+                        handleToeflMarketingRedirect(marketingObj);
                     }
                 });
+            }
+            else {
+                var APP_WORKOUTS_STATE = 'app.workouts.roadmap';
+                var isGoingToWorkoutsState = toState.name.indexOf(APP_WORKOUTS_STATE) !== -1;
+
+                if (isGoingToWorkoutsState) {
+                    evt.preventDefault();
+
+                    OnBoardingService.isOnBoardingCompleted().then(function (_isOnBoardingCompleted) {
+                        isOnBoardingCompleted = _isOnBoardingCompleted;
+
+                        if (!isOnBoardingCompleted) {
+                            var ON_BOARDING_STATE_NAME = 'app.onBoarding';
+                            var isNotFromOnBoardingState = fromState.name.indexOf(ON_BOARDING_STATE_NAME) === -1;
+                            if (isNotFromOnBoardingState) {
+                                $state.go(ON_BOARDING_STATE_NAME);
+                            }
+                        } else {
+                            OnBoardingService.getMarketingToefl().then(function (marketingObj) {
+                                // 7 - app status
+                                if (marketingObj && marketingObj.status && marketingObj.status !== 7) {
+                                    handleToeflMarketingRedirect(marketingObj);
+                                } else {
+                                    $state.go(toState, toParams, {
+                                        reload: true
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+
+            function handleToeflMarketingRedirect(marketingObj) {
+                var state = '';
+                switch (marketingObj.status) {
+                    case 2:
+                        state = 'email';
+                        break;
+                    case 3:
+                        state = 'verifyEmail';
+                        break;
+                    case 4:
+                        state = 'purchase';
+                        break;
+                    case 5:
+                        state = 'purchase';
+                        break;
+                    case 6:
+                        state = 'signup';
+                        break;
+                    default:
+                        state = 'purchase';
+                }
+                window.location.href = `${ENV.zinkerzWebsiteBaseUrl}myzinkerz/toefl/${state}`;
             }
         });
     });
