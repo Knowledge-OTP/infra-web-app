7e7dca3f996675355c054752aa2be60a15189b90
OfirNadav <ofir.nadav@gmail.com>
2018-06-11 16:00:40 +0300

    elipsisactivate OneSignal

---
+++ src/components/oneSignal/run/activateOneSignal.run.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular.module('znk.infra-web-app.oneSignal').run(
+    function (OneSignalService) {
+      'ngInject';
+
+        OneSignalService.initOneSignal();
+
+        OneSignalService.sendTag("App", "MyZinkerz", (res) => {
+            console.log('tags recieved by server', res);
+        });
+    });
+})();
