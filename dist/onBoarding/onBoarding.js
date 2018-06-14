5c73e0136c669ed248823897274f483d5cfa3815
choroshin <choroshin@gmail.com>
2018-06-14 16:46:14 +0300

    save

--- dist/onBoarding/onBoarding.js
+++ dist/onBoarding/onBoarding.js
@@ -304,49 +304,46 @@
         $rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState) {//eslint-disable-line
             evt.preventDefault();
             OnBoardingService.getMarketingToefl().then(function (marketingObj) {
-                if (isOnBoardingCompleted) {
                     if (marketingObj && marketingObj.status) {
                         OnBoardingService.updatePage(toState.name);
                     }
-                    $state.go(toState, toParams, {
-                        reload: true
-                    });
-                }
-                else {
-                    // statuses:  7 - app  , 1 - diagnostic
-                    if (marketingObj && marketingObj.status && marketingObj.status !== 1 && marketingObj.status !== 7 && toState.name !== 'app.diagnostic.preSummary') {
-                        handleToeflMarketingRedirect(marketingObj);
-                    } else {
-                        var APP_WORKOUTS_STATE = 'app.workouts.roadmap';
-                        var isGoingToWorkoutsState = toState.name.indexOf(APP_WORKOUTS_STATE) !== -1;
+                // statuses:  7 - app  , 1 - diagnostic
+                if (marketingObj && marketingObj.status && marketingObj.status !== 1 && marketingObj.status !== 7 && toState.name !== 'app.diagnostic.preSummary') {
+                    handleToeflMarketingRedirect(marketingObj);
+                } else {
+                    var APP_WORKOUTS_STATE = 'app.workouts.roadmap';
+                    var isGoingToWorkoutsState = toState.name.indexOf(APP_WORKOUTS_STATE) !== -1;
 
-                        if (isGoingToWorkoutsState) {
-                            evt.preventDefault();
+                    if (isGoingToWorkoutsState) {
+                        evt.preventDefault();
 
-                            OnBoardingService.isOnBoardingCompleted().then(function (_isOnBoardingCompleted) {
-                                isOnBoardingCompleted = _isOnBoardingCompleted;
+                        OnBoardingService.isOnBoardingCompleted().then(function (_isOnBoardingCompleted) {
+                            isOnBoardingCompleted = _isOnBoardingCompleted;
 
-                                if (!isOnBoardingCompleted) {
-                                    var ON_BOARDING_STATE_NAME = 'app.onBoarding';
-                                    var isNotFromOnBoardingState = fromState.name.indexOf(ON_BOARDING_STATE_NAME) === -1;
-                                    if (isNotFromOnBoardingState) {
-                                        if (marketingObj && marketingObj.status) {
-                                            OnBoardingService.updatePage(ON_BOARDING_STATE_NAME);
-                                        }
-                                        $state.go(ON_BOARDING_STATE_NAME);
-                                    }
-                                } else {
+                            if (!isOnBoardingCompleted) {
+                                var ON_BOARDING_STATE_NAME = 'app.onBoarding';
+                                var isNotFromOnBoardingState = fromState.name.indexOf(ON_BOARDING_STATE_NAME) === -1;
+                                if (isNotFromOnBoardingState) {
                                     if (marketingObj && marketingObj.status) {
-                                        OnBoardingService.updatePage(toState.name);
+                                        OnBoardingService.updatePage(ON_BOARDING_STATE_NAME);
                                     }
-                                    $state.go(toState, toParams, {
-                                        reload: true
-                                    });
+                                    $state.go(ON_BOARDING_STATE_NAME);
                                 }
-                            });
-                        }
+                            } else {
+                                if (marketingObj && marketingObj.status) {
+                                    OnBoardingService.updatePage(toState.name);
+                                }
+                                $state.go(toState, toParams, {
+                                    reload: true
+                                });
+                            }
+                        });
                     }
                 }
+                $state.go(toState, toParams, {
+                    reload: true
+                });
+
 
                 function handleToeflMarketingRedirect(marketingObj) {
                     var state = '';
