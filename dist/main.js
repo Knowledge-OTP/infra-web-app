416ebde904b8c4d6e6468ff49fb4cd850b3b5be9
Ariel-W <ariel.weiss@gmail.com>
2018-09-13 14:26:02 +0300

    dist (#566)

--- dist/main.js
+++ dist/main.js
@@ -10660,13 +10660,8 @@ angular.module('znk.infra-web-app.liveLessons').run(['$templateCache', function 
                             if (liveSessionData.studentId === currUid) {
                                 LiveSessionUiSrv.showStudentConfirmationPopUp()
                                     .then((resolveReason) => {
-<<<<<<< HEAD
                                         // Making sure the user actually clicked "JOIN" and we did not close the popup from the code automatically
                                         if (resolveReason && resolveReason.toLowerCase() === 'join') {
-=======
-                                        // Making sure the user actually clicked "Accept" / "JOIN" and we did not close the popup from the code automatically
-                                        if (resolveReason === 'CANCEL') {
->>>>>>> zinkerz/dev
                                             LiveSessionSrv.confirmLiveSession(liveSessionData.guid);
                                         }
                                     }, () => {
@@ -10675,11 +10670,7 @@ angular.module('znk.infra-web-app.liveLessons').run(['$templateCache', function 
                             } else {
                                 LiveSessionUiSrv.showEducatorPendingPopUp().then((resolveReason) => {
                                     // Making sure the user actually clicked "CANCEL" (only button in this popup) and we did not close the popup from the code automatically
-<<<<<<< HEAD
                                     if (resolveReason && resolveReason.toLowerCase() === 'cancel') {
-=======
-                                    if (resolveReason === 'CANCEL') {
->>>>>>> zinkerz/dev
                                         LiveSessionSrv.endLiveSession(liveSessionData.guid);
                                     }
                                 });
